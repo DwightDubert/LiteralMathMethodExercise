8c21c6dcc260df6c862532b8885433c163d913ba
DwightDubert <dwight.dubert@stu.caverna.kyschools.us>
2022-02-15 14:22:37 -0600

    Iaaaaa

---
+++ script.js
@@ -0,0 +1,15 @@
+let warmHugs = "Hi i am like hug"
+console.log(warmHugs.toUpperCase());
+warmHugs = warmHugs.replace("like", "love")
+
+let beenImpaled = "Oh no i seem to have been impaled"
+console.log(beenImpaled.slice(16,36));
+
+let dotDotDot = "..."
+
+let skullBones = `I don't have skin${dotDotDot}or bones`
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.floor((Math.random()*3))+1
